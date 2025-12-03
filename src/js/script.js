d4d197bfe40dc7a9754395092254b17ce6437aad
“William” <wihezuo26@aoiths.org>
2025-12-03 13:34:07 -0500

    finished

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,11 @@
 
 // CREATE EVENT LISTENERS
+// Basic loop structure
+function runLoop  (start, end, step,direction) 
 
+// Example usage
+let loopResult = runLoop(1, 15, 1, 'none'); // Count up from 1 to 15
+document.querySelector("#resultDisplay").innerHTML = loopResult.join(", ");
 
 
 
